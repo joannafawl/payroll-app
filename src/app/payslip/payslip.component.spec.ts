b30913918f48eb1ebe0f69b2351f953c4b954b37
joannafawl <joanna.fawl@madetech.com>
2020-12-15 15:56:48 +0000

    Add and pass tests to display pension and national insurance

--- src/app/payslip/payslip.component.spec.ts
+++ src/app/payslip/payslip.component.spec.ts
@@ -44,4 +44,52 @@ describe('PayslipComponent', () => {
     const text = screen.getByTestId('grossPayMonth');
     expect(text.textContent).toContain("£3500");
   })
+
+  it('should calculate and display monthly salary after tax for high salary', async () => {
+    await render(PayslipComponent, {
+      componentProperties: { user: { id: 1, name: "Joanna", salary: 30000 } }
+    })
+    const text = screen.getByTestId('afterTax');
+    expect(text.textContent).toContain("£2083.33");
+  })
+
+  it('should calculate and display monthly salary after tax for low salary', async () => {
+    await render(PayslipComponent, {
+      componentProperties: { user: { id: 1, name: "Joanna", salary: 8000 } }
+    })
+    const text = screen.getByTestId('afterTax');
+    expect(text.textContent).toContain("£666.67");
+  })
+
+  it('should calculate and display monthly salary after tax for medium salary', async () => {
+    await render(PayslipComponent, {
+      componentProperties: { user: { id: 1, name: "Joanna", salary: 22500 } }
+    })
+    const text = screen.getByTestId('afterTax');
+    expect(text.textContent).toContain("£1666.67");
+  })
+  
+  it('should calculate and display pension from monthly salary - £22500', async () => {
+    await render(PayslipComponent, {
+      componentProperties: { user: { id: 1, name: "Joanna", salary: 22500 } }
+    })
+    const text = screen.getByTestId('pension');
+    expect(text.textContent).toContain("£100");
+  })
+
+  it('should calculate and display pension from large monthly salary - £200,000', async () => {
+    await render(PayslipComponent, {
+      componentProperties: { user: { id: 2, name: "Duncan", salary: 200000 } }
+    })
+    const text = screen.getByTestId('pension');
+    expect(text.textContent).toContain("£635");
+  })
+
+  it('should calculate and display national insurance from large monthly salary - £22500', async () => {
+    await render(PayslipComponent, {
+      componentProperties: { user: { id: 2, name: "Duncan", salary: 22500 } }
+    })
+    const text = screen.getByTestId('nationalInsurance');
+    expect(text.textContent).toContain("£135");
+  })
 });
--- src/app/payslip/payslip.component.ts
+++ src/app/payslip/payslip.component.ts
@@ -12,21 +12,43 @@ import { Observable } from 'rxjs';
 export class PayslipComponent implements OnInit {
 
   @Input() user: User
+  monthlyPayAfterTax: number;
 
   constructor() { }
 
   ngOnInit(): void {
-    // get() {
-    //   // const getOptions = {
-    //   //   params: { id }
-    //   // };
-      // this.user = this.http.get<User>('127.0.0.1:4201');
+    this.monthlyPayAfterTax = this.calculateMonthlyPayAfterTax();
   }
 
   calculateMonthlyGrossPay(): number {
     return this.user.salary / 12;
   }
 
+  calculateMonthlyPayAfterTax(): number {
+    const taxFreePayDeducted = this.user.salary - 10000;
+    let lowerRateTaxPay = 0;
+    let upperRateTaxPay = 0;
+    if (taxFreePayDeducted < 0) {
+      return (Math.round((this.user.salary / 12) * 100) / 100);
+    }
+    else if (taxFreePayDeducted < 15000) {
+      lowerRateTaxPay = taxFreePayDeducted * 0.2;
+    }
+    else {
+      lowerRateTaxPay = 15000 * 0.2;
+      upperRateTaxPay = (taxFreePayDeducted - 15000) * 0.4;
+    }
+    return (Math.round(((this.user.salary - lowerRateTaxPay - upperRateTaxPay) / 12) * 100) / 100);
+  }
+
+  calculatePensionContribution(): number {
+    return Math.round(this.monthlyPayAfterTax * 0.06 * 100) / 100;
+  }
+
+  calculateNationalInsuranceContribution(): number {
+    return Math.round(((this.user.salary - 9000) * 0.12) /12 * 100) / 100;
+
+  }
 }
 
 
