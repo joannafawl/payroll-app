2e6af8e3f1e17811c03fc4d5ffc1098e093760d0
joannafawl <joanna.fawl@madetech.com>
2020-12-15 17:02:13 +0000

    Add PayslipComponent to declarations in app.component.spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { ConstantPool } from '@angular/compiler';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PayslipComponent } from './payslip/payslip.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -10,7 +11,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        PayslipComponent
       ],
     }).compileComponents();
   });
